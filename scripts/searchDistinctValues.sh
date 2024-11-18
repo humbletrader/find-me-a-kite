b98bed7cf0a981b55e6a41bac3e53664416eb7ae
dragos <dragos.balan@adswizz.com>
2024-11-19 00:42:11 +0200

    scripts updated

--- scripts/searchDistinctValues.sh
+++ scripts/searchDistinctValues.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-APP_HOST=localhost
-APP_PORT=8081
+APP_HOST=find-me-a-kite.herokuapp.com
+APP_PORT=80
 
 printf "\n\n*********************************************************"
 printf "\n        searching for distinct values on ${APP_HOST} "
@@ -9,4 +9,4 @@ printf "\n**********************************************************\n"
 curl -v http://$APP_HOST:$APP_PORT/searchDistinctValues \
      --header "Content-Type: application/json" \
      --request POST \
-     --data '{ "target" : "subprod_name", "criteria" : {"category": "KITES", "country": "EU", "brand": "duotone"}}'
+     --data '{"target":"brand","criteria":{"category":{{"value":"KITES","op":"eq"}},"country":{{"value":"EU","op":"eq"}}}}'
